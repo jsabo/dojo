54679f5278678729a378bd65e7ea55a426cdf1c0
Jonathan Sabo <jonathan@sabo.io>
2021-03-10 15:06:11 -0500

    wip

--- 01-configure-peering-with-spine-leaf-solo-tor-k8s-as-per-rack/04-ippool-disable-encap-and-nat-outgoing.sh
+++ 01-configure-peering-with-spine-leaf-solo-tor-k8s-as-per-rack/04-ippool-disable-encap-and-nat-outgoing.sh
@@ -1,4 +1,6 @@
 calicoctl get ippool default-ipv4-ippool -o yaml
+echo
 calicoctl patch ippool default-ipv4-ippool --patch '{"spec":{"ipipMode": "Never"}}'
 calicoctl patch ippool default-ipv4-ippool --patch '{"spec":{"natOutgoing": false}}'
+echo
 calicoctl get ippool default-ipv4-ippool -o yaml
--- 99-reset/enable-encap-and-mesh-peering.sh
+++ 99-reset/enable-encap-and-mesh-peering.sh
@@ -1,3 +1,10 @@
+calicoctl get bgpconfiguration default -o yaml
+echo
 calicoctl patch bgpconfiguration default --patch '{"spec":{"nodeToNodeMeshEnabled": true}}'
+echo
+calicoctl get ippool default-ipv4-ippool -o yaml
+echo
 calicoctl patch ippool default-ipv4-ippool --patch '{"spec":{"ipipMode": "Always"}}'
 calicoctl patch ippool default-ipv4-ippool --patch '{"spec":{"natOutgoing": true}}'
+echo
+calicoctl get ippool default-ipv4-ippool -o yaml
