823c311f28a55dfd4796637cc447693de2728c21
Jonathan Sabo <jonathan@sabo.io>
2021-03-10 15:05:49 -0500

    wip

--- 02-advertise-kubernetes-service-ip-addresses/00-advertise-service-clustr-ip-cidr.sh
+++ 02-advertise-kubernetes-service-ip-addresses/00-advertise-service-clustr-ip-cidr.sh
@@ -1,5 +1,7 @@
 ## Advertise service cluster IP addresses
 
 calicoctl get bgpconfiguration default -o yaml
+echo
 calicoctl patch bgpconfiguration default -p '{"spec":{"serviceClusterIPs": [{"cidr": "10.49.0.0/16"}]}}'
+echo
 calicoctl get bgpconfiguration default -o yaml
